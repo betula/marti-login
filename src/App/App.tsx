1c55ff39b2c5415fd52a383e6dd0000ebb00007d
Slava Bereza <mail@betula.co>
2023-08-15 20:50:37 +0300

    chore: hash router

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,11 @@
-import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Navigate, RouterProvider, createHashRouter } from 'react-router-dom';
 import { SignIn } from '../components/auth/SignIn/SignIn';
 import { SignUp } from '../components/auth/SignUp/SignUp';
 import { Forgot } from '../components/auth/Forgot/Forgot';
 import { Welcome } from '../components/Welcome/Welcome';
 import { Page } from '../components/Page/Page';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/sign-in',
     element: <Page><SignIn /></Page>
